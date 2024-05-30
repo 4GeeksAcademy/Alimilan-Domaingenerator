3d93ad8cb14e34ca24095cbd6bbb112ebbf4746f
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-05-30 19:14:55 +0000

    prettier off

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let tag = document.querySelector("#the-domaim");
+  tag.innerHTML = generateDomain();
+  function generateDomain() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "raccoon"];
+    let ext = [".es", ".com"];
+
+    let domain = "hola";
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < ext.length; l++) {
+            domain = pronoun[i] + adj[j] + noun[k] + ext[l];
+          }
+        }
+      }
+    }
+    console.log(domain);
+    return domain;
+  }
 };
