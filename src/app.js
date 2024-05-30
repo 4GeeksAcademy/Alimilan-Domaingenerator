df69fdfe04bfcca04eccd196457853c7ee22a24e
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-05-30 20:04:50 +0000

    Domain genrator ready

--- src/app.js
+++ src/app.js
@@ -6,26 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let tag = document.querySelector("#the-domaim");
+  let tag = document.querySelector("#the-domain");
   tag.innerHTML = generateDomain();
+
   function generateDomain() {
     let pronoun = ["the", "our"];
     let adj = ["great", "big"];
     let noun = ["jogger", "raccoon"];
     let ext = [".es", ".com"];
 
-    let domain = "hola";
+    let domain = "";
 
     for (let i = 0; i < pronoun.length; i++) {
       for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
           for (let l = 0; l < ext.length; l++) {
-            domain = pronoun[i] + adj[j] + noun[k] + ext[l];
+            domain += pronoun[i] + adj[j] + noun[k] + ext[l] + "<br>";
+      
           }
         }
       }
     }
-    console.log(domain);
+
     return domain;
   }
 };
+
+
